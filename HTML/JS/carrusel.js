31c6b7f0df1bb5458682d9f7819bb49add01d737
JorgeSanchez28 <jorgesanchezortiz2812@gmail.com>
2025-05-26 08:47:27 +0200

    .

--- HTML/JS/carrusel.js
+++ HTML/JS/carrusel.js
@@ -1,9 +1,11 @@
 let indice = 0;
 
 function mover(direccion) {
-  const total = 5;
+  const total = 3;
   indice = (indice + direccion + total) % total;
   document.getElementById("imagenes").style.transform = `translateX(-${indice * 100}%)`;
 }
 
-setInterval(() => mover(1), 5000);
+setInterval(function() {
+  cambiarImagen(1);
+}, 20000);
